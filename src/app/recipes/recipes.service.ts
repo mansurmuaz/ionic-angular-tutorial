02b4d3994da839afa1052b5aa653aff1e23b28e8
Mansur Muaz Ekici <mmuazekici@gmail.com>
2019-06-19 16:46:01 +0200

    List view and details

---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+
+    private recipes: Recipe[] = [
+        {
+            id: 'r1',
+            title: 'Menemen',
+            imageUrl: 'https://www.mensjournal.com/wp-content/uploads/mf/1280-turkish-menemen.jpg?w=1200&h=1200&crop=1',
+            ingredients: ['tomato', 'pepper', 'egg', 'cheese']
+        },
+        {
+            id: 'r2',
+            title: 'Sucuklu Yumurta',
+            imageUrl: 'http://sutis.com.tr/Upload/Menu/Buyuk/1432019-Sucuklu-Yumurta-1490.jpg',
+            ingredients: ['egg', 'sucuk', 'salt']
+        }
+    ];
+
+    constructor() { }
+
+    getAllRecipes() {
+        return [...this.recipes];
+    }
+
+    getRecipe(recipeId: string) {
+        return {
+            ...this.recipes.find( recipe => {
+            return recipe.id === recipeId;
+        })};
+    }
+}
